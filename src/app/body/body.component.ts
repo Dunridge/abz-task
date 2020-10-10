b72ca60886f3035423bb29c103726478de9bd848
Dunridge <Polinka7max@gmail.com>
2020-10-10 22:40:28 +0300

    added file upload management

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -1,9 +1,10 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
 import {User} from '../../interfaces/user';
 import {USERS} from '../../model/users';
 import {UserForm} from '../../interfaces/user-form';
 import {FormBuilder, FormGroup} from '@angular/forms';
 import {POSITIONS} from '../../model/positions';
+import {DOCUMENT} from '@angular/common';
 
 @Component({
   selector: 'app-body',
@@ -20,7 +21,12 @@ export class BodyComponent implements OnInit {
   feedback: UserForm;
   feedbackForm: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  selectedFile: File;
+
+  constructor(
+    private fb: FormBuilder,
+    @Inject(DOCUMENT) private document: Document
+  ) {
     this.createForm();
   }
 
@@ -41,10 +47,14 @@ export class BodyComponent implements OnInit {
     this.feedback = this.feedbackForm.value;
     this.feedback.position = this.positions[this.selection];
     console.log(this.feedback);
+    // TODO: now add this to the form or send it to the server -- read the API docs (this.selectedFile)
+    console.log(this.selectedFile);
     this.feedbackForm.reset();
   }
 
-  uploadPhoto(): void {
-    console.log('photo uploaded');
+  onFileChanged($event: Event): void {
+    console.log($event);
+    // @ts-ignore
+    this.selectedFile = $event.target.files[0];
   }
 }
