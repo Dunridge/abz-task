7feba61c551983e4131d81cb440a74d78506fd04
Dunridge <Polinka7max@gmail.com>
2020-10-11 22:50:19 +0300

    fixed the header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostListener, Inject, OnInit} from '@angular/core';
+import {DOCUMENT} from '@angular/common';
 
 @Component({
   selector: 'app-header',
@@ -6,8 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  public navFixed = false;
+  private scrollOffset = 70;
 
-  constructor() { }
+
+  @HostListener('window: scroll')
+  private onWindowScroll(): void {
+    this.navFixed = (window.pageYOffset
+      || document.documentElement.scrollTop
+      || document.body.scrollTop || 0
+    ) > this.scrollOffset;
+    if (window.pageYOffset === 72) {
+      this.navFixed = false;
+      document.getElementById('header-element').style.position = 'static';
+    } else {
+      // document.getElementById('header-element').style.position = 'fixed';
+      const headerElem = document.getElementById('header-element');
+      headerElem.style.position = 'fixed';
+      // headerElem.style.zIndex = '100';
+    }
+  }
+
+  constructor(
+    @Inject(DOCUMENT) document: any
+  ) {
+  }
 
   ngOnInit(): void {
   }
