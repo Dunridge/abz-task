a2aafe3c57bc89357dac3aa4378b058b77bed15b
Dunridge <Polinka7max@gmail.com>
2020-10-11 19:24:23 +0300

    added the registration component

--- src/app/body/sections/registration/registration.component.ts
+++ src/app/body/sections/registration/registration.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
+import {User} from '../../../../interfaces/user';
+import {USERS} from '../../../../model/users';
+import {POSITIONS} from '../../../../model/positions';
+import {UserForm} from '../../../../interfaces/user-form';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {DOCUMENT} from '@angular/common';
 
 @Component({
   selector: 'app-registration',
@@ -7,9 +13,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+  users: User[] = USERS;
+  positions: string[] = POSITIONS;
+  public selection = '';
+  public defaultSelected = 'Frontend developer';
+
+  feedback: UserForm;
+  feedbackForm: FormGroup;
+
+  selectedFile: File;
+
+  constructor(
+    private fb: FormBuilder,
+    @Inject(DOCUMENT) private document: Document
+  ) {
+    this.createForm();
+  }
 
   ngOnInit(): void {
+    console.log(this.users);
+  }
+
+  private createForm(): void {
+    this.feedbackForm = this.fb.group({
+      name: '',
+      email: '',
+      phoneNumber: '',
+      position: ''
+    });
+  }
+
+  onSubmit(): void {
+    this.feedback = this.feedbackForm.value;
+    this.feedback.position = this.positions[this.selection];
+    console.log(this.feedback);
+    // TODO: now add this to the form or send it to the server -- read the API docs (this.selectedFile)
+    console.log(this.selectedFile);
+    this.feedbackForm.reset();
+  }
+
+  onFileChanged($event: Event): void {
+    console.log($event);
+    // @ts-ignore
+    this.selectedFile = $event.target.files[0];
   }
 
 }
